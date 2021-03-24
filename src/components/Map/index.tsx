4fa07c3702fcaf4297ea18c8702007ac543bc05f
Rafael <rafa.shiga@gmail.com>
2021-03-24 13:20:43 -0300

    feat: add zoom on mobile

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/dist/client/router'
-import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, MapConsumer } from 'react-leaflet'
 import * as S from './styles'
 
 interface Place {
@@ -50,6 +50,18 @@ const Map = ({ places }: MapProps) => {
         style={{ height: '100%', width: '100%' }}
         attributionControl={false}
       >
+        <MapConsumer>
+          {(map) => {
+            const width =
+              window.innerWidth ||
+              document.documentElement.clientWidth ||
+              document.body.clientWidth
+            if (width < 768) {
+              map.setMinZoom(2)
+            }
+            return null
+          }}
+        </MapConsumer>
         <CustomTileLayer />
 
         {places?.map(({ id, name, slug, location }) => {
